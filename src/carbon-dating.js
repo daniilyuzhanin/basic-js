715b63b30aba074d107b923f66e7e470b9c3da0c
Daniil Yuzhanin <daniilyuzhanin@gmail.com>
2020-11-01 16:55:57 +0300

    feat: cats + carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,13 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+	let str = parseFloat(sampleActivity, 10);
+	let number = false;
+	if (typeof sampleActivity == 'String' && str > 0 && str < 15) {
+		number = Math.ceil(Math.log(MODERN_ACTIVITY / str) / (0.693 / HALF_LIFE_PERIOD));
+		return number;
+	}
+	return number;
+	// ТЕСТЫ ПРОХОДЯТ БОЛЬШЕ 60 СЕКУНД
 };
